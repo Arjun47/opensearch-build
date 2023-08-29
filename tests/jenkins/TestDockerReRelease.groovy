61bccc5658193ad00d37171cffd63c95a0f50210
Divya Madala <divyaasm@amazon.com>
2023-08-29 08:49:31 -0700

    Update
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- tests/jenkins/TestDockerReRelease.groovy
+++ tests/jenkins/TestDockerReRelease.groovy
@@ -7,6 +7,7 @@
 import jenkins.tests.BuildPipelineTest
 import org.junit.Before
 import org.junit.Test
+import org.yaml.snakeyaml.Yaml
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import static com.lesfurets.jenkins.unit.global.lib.GitSource.gitSource
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
@@ -35,11 +36,23 @@ class TestDockerReRelease extends BuildPipelineTest {
         binding.setVariable('PRODUCT', 'opensearch')
         binding.setVariable('TAG', '1')
         binding.setVariable('RE_RELEASE', 'true')
+        helper.addShMock("""docker inspect --format '{{ index .Config.Labels "org.label-schema.version"}}' opensearchproject/opensearch:1""") { script ->
+            return [stdout: "1.3.12", exitValue: 0]
+        }
+        helper.addShMock("""docker inspect --format '{{ index .Config.Labels "org.label-schema.description"}}' opensearchproject/opensearch:1""") { script ->
+            return [stdout: "7756", exitValue: 0]
+        }
+        helper.addShMock("""docker inspect --format '{{ index .Config.Labels "org.label-schema.build-date"}}' opensearchproject/opensearch:1""") { script ->
+            return [stdout: "2023-06-19T19:12:59Z", exitValue: 0]
+        }
+        helper.addShMock("""docker inspect --format '{{ index .Config.Labels "org.label-schema.version"}}' opensearchproject/opensearch:latest""") { script ->
+            return [stdout: "2.5.0", exitValue: 0]
+        }
 
     }
 
     @Test
-    void DockerScan_test() {
+    void testDocker() {
         super.testPipeline('jenkins/docker/docker-re-release.jenkinsfile',
                 'tests/jenkins/jenkinsjob-regression-files/docker/docker-re-release.jenkinsfile')
     }
