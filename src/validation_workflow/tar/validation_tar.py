6b877efd307efbc1d206fe1aa181eef400b4aaf3
Divya Madala <divyaasm@amazon.com>
2023-09-07 12:47:51 -0700

    Changes updated
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/tar/validation_tar.py
+++ src/validation_workflow/tar/validation_tar.py
@@ -48,10 +48,10 @@ class ValidateTar(Validation, DownloadUtils):
             else:
                 if (self.args.artifact_type == "staging"):
                     self.args.file_path[project] = f"{self.base_url_staging}{project}/{self.args.version}/{self.args.build_number[project]}/linux/{self.args.arch}/{self.args.distribution}/dist/{project}/{project}-{self.args.version}-linux-{self.args.arch}.tar.gz"  # noqa: E501
-                    print("name": project)
+                    print("name:" + project)
                 else:
                     self.args.file_path[project] = f"{self.base_url_production}{project}/{self.args.version}/{project}-{self.args.version}-linux-{self.args.arch}.tar.gz"
-                    print("name": project)
+                    print("name:" + project)
                 print(self.args.file_path[project])
                 self.check_url(self.args.file_path.get(project))
         return True
