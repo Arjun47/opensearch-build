cf39f228cc4023e219224338d8196a3ceb105441
Divya Madala <divyaasm@amazon.com>
2023-09-08 09:58:24 -0700

    Changes updated
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation_args.py
+++ src/validation_workflow/validation_args.py
@@ -126,14 +126,14 @@ class ValidationArgs:
 
         if (not (args.version or args.file_path)):
             raise Exception("Provide either version number or File Path")
+        if (("opensearch" not in args.file_path.keys()) or ('opensearch' not in args.projects)):
+            raise Exception("Provide OpenSearch Artifact details along with OpenSearch DashBoards to validate")
         if(args.file_path):
             print(type(args.file_path))
             args.distribution = self.get_distribution_type(args.file_path.values())
             if("opensearch-dashboards" in args.file_path.keys()):
                 args.projects.append("opensearch-dashboards")
         print(args.projects)
-        if (('opensearch' not in args.projects) or ("opensearch" not in args.file_path.keys())):
-            raise Exception("Provide OpenSearch Artifact details along with OpenSearch DashBoards to validate")
 
         self.version = args.version
         self.file_path = args.file_path
