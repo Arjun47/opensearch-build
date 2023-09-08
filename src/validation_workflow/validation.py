1a732871adf0d8f5619ef6ecb1ba402053f163db
Divya Madala <divyaasm@amazon.com>
2023-09-08 11:27:36 -0700

    Changes updated
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation.py
+++ src/validation_workflow/validation.py
@@ -22,7 +22,7 @@ class Validation(ABC):
         self.args = args
 
     def check_url(self, url: str) -> bool:
-        if self.download(url, self.tmp_dir):  # type: ignore
+        if (self.is_url_valid(url) and self.download(url, self.tmp_dir)):  # type: ignore
             print(url)
             logging.info(f"Valid URL - {url} and Download Successful !")
             return True
