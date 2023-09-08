b8a36f159089e220d5bd652e5d4880a50af623b5
Divya Madala <divyaasm@amazon.com>
2023-09-08 10:47:26 -0700

    Changes updated
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/tar/validation_tar.py
+++ src/validation_workflow/tar/validation_tar.py
@@ -40,6 +40,7 @@ class ValidateTar(Validation, DownloadUtils):
         for project in self.args.projects:
             print(project)
             print("name:" + project)
+            print(self.args.version)
             if (self.args.version != ""):
                 if ("https:" not in self.args.file_path.get(project)):
                     self.copy_artifact(self.args.file_path.get(project), str(self.tmp_dir.path))
