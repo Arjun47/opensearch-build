60e93b9df765a73e9e44494f2bceb12e55e4c420
Divya Madala <divyaasm@amazon.com>
2023-09-11 17:28:04 -0700

    Update docker
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/validation_args.py
+++ src/validation_workflow/validation_args.py
@@ -101,7 +101,7 @@ class ValidationArgs:
             nargs='+',
             help="Enter type of projects to be validated",
             choices=["opensearch", "opensearch-dashboards"],
-            default=["opensearch-dashboards", "opensearch"]
+            default=['opensearch-dashboards', 'opensearch']
         )
         parser.add_argument(
             "--artifact-type",
