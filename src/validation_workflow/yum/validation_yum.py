e3b1c40a354b7c189afab666b80c040999a46a9e
Divya Madala <divyaasm@amazon.com>
2023-09-11 22:57:04 -0700

    Update docker
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/yum/validation_yum.py
+++ src/validation_workflow/yum/validation_yum.py
@@ -7,6 +7,7 @@
 
 import logging
 import time
+import os
 
 from system.execute import execute
 from system.temporary_directory import TemporaryDirectory
@@ -20,26 +21,56 @@ class ValidateYum(Validation, DownloadUtils):
 
     def __init__(self, args: ValidationArgs) -> None:
         super().__init__(args)
-        self.base_url = "https://artifacts.opensearch.org/releases/bundle/"
+        self.base_url_production = "https://artifacts.opensearch.org/releases/bundle/"
+        self.base_url_staging = "https://ci.opensearch.org/ci/dbc/distribution-build-"
         self.tmp_dir = TemporaryDirectory()
 
         # shutil.copy2(localfilepath, temporaryfile_path)
 
     def download_artifacts(self) -> bool:
+        isFilePathEmpty = bool(self.args.file_path)
         for project in self.args.projects:
-            url = f"{self.base_url}{project}/{self.args.version[0:1]}.x/{project}-{self.args.version[0:1]}.x.repo"
-            self.check_url(url)
+            print(project)
+            print("name:" + project)
+            print(self.args.version)
+            if (isFilePathEmpty):
+                if ("https:" not in self.args.file_path.get(project)):
+                    self.copy_artifact(self.args.file_path.get(project), str(self.tmp_dir.path))
+                else:
+                    self.check_url(self.args.file_path.get(project))
+            else:
+                if (self.args.artifact_type == "staging"):
+                    self.args.file_path[project] = f"{self.base_url_staging}{project}/{self.args.version}/{self.args.build_number[project]}/linux/{self.args.arch}/rpm/dist/{project}/{project}-{self.args.version}-linux-{self.args.arch}.staging.repo"  # noqa: E501
+                    print("name:" + project)
+                else:
+                    self.args.file_path[project] = f"{self.base_url_production}{project}/{self.args.version[0:1]}.x/{project}-{self.args.version[0:1]}.x.repo"
+                    print("name:" + project)
+                print(self.args.file_path[project])
+                self.check_url(self.args.file_path.get(project))
         return True
 
     def installation(self) -> bool:
         try:
             execute('sudo rpm --import https://artifacts.opensearch.org/publickeys/opensearch.pgp', str(self.tmp_dir.path), True, False)
             for project in self.args.projects:
+                self.filename = os.path.basename(self.args.file_path.get(project))
+                execute(f'sudo yum remove {project} -y', ".")
+                print(self.filename)
+                execute(f'sudo rpm -ivh {os.path.join(self.tmp_dir.path, self.filename)}', str(self.tmp_dir.path), True, False)
+        except:
+            raise Exception('Failed to Install Opensearch')
+        return True
+
+    def installation(self) -> bool:
+        try:
+            execute('sudo rpm --import https://artifacts.opensearch.org/publickeys/opensearch.pgp', str(self.tmp_dir.path), True, False)
+            for project in self.args.projects:
+                self.filename = os.path.basename(self.args.file_path.get(project))
                 execute(f'sudo yum remove {project} -y', ".")
                 logging.info('Removed previous versions of Opensearch')
-                urllink = f"{self.base_url}{project}/{self.args.version[0:1]}.x/{project}-{self.args.version[0:1]}.x.repo -o /etc/yum.repos.d/{project}-{self.args.version[0:1]}.x.repo"
+                urllink = f"{self.args.file_path.get(project)}.x.repo -o /etc/yum.repos.d/{os.path.basename(self.args.file_path.get(project))}"
                 execute(f'sudo curl -SL {urllink}', ".")
-                execute(f"sudo yum install '{project}-{self.args.version}' -y", ".")
+                execute(f"sudo yum install '{project}' -y", ".")
         except:
             raise Exception('Failed to install Opensearch')
         return True
