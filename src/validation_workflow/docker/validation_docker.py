2e8f22ec5fb55aa38bbd57a6caf2df72cb439f4e
Divya Madala <divyaasm@amazon.com>
2023-09-11 17:05:15 -0700

    Update docker
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/docker/validation_docker.py
+++ src/validation_workflow/docker/validation_docker.py
@@ -39,10 +39,10 @@ class ValidateDocker(Validation):
             get_image_id = lambda product: self.get_image_id(  # noqa: E731
                 self.get_artifact_image_name(product, using_staging_artifact_only),
                 self.args.version if not self.args.using_staging_artifact_only else ValidationArgs().stg_tag(product).replace(" ", ""))
-            self.image_ids = list(map(get_image_id, product_names))
-            for i in self.image_ids:
+            image_ids = list(map(get_image_id, product_names))
+            for i in image_ids:
                 print(i)
-            self.image_ids = {key: value for key, value in zip(product_names, self.image_ids)}
+            self.image_ids = {key: value for key, value in zip(product_names, image_ids)}
             print(self.image_ids)
 
             return True
