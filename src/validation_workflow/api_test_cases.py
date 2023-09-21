7a5d841354f1dfa08df033ae638ce7d52344694e
Divya Madala <divyaasm@amazon.com>
2023-09-20 22:29:20 -0700

    back to previous apitestcases method
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/api_test_cases.py
+++ src/validation_workflow/api_test_cases.py
@@ -20,7 +20,6 @@ class ApiTestCases:
     def __init__(self) -> None:
         pass
 
-    @staticmethod
     def test_cases(self, projects: list) -> Any:
         pass_counter, fail_counter = 0, 0
 
