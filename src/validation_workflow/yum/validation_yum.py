bd68786c0e64b985e85f0a46019c75d661fa7445
Divya Madala <divyaasm@amazon.com>
2024-01-26 12:02:44 -0800

    Final changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/yum/validation_yum.py
+++ src/validation_workflow/yum/validation_yum.py
@@ -53,7 +53,7 @@ class ValidateYum(Validation, DownloadUtils):
                 logging.info('Removed previous versions of Opensearch')
                 urllink = f"{self.args.file_path.get(project)} -o /etc/yum.repos.d/{os.path.basename(self.args.file_path.get(project))}"
                 execute(f'sudo curl -SL {urllink}', ".")
-                execute(f"sudo yum install '{project}-{self.args.version}' -y", ".")
+                execute(f"sudo env OPENSEARCH_INITIAL_ADMIN_PASSWORD={get_password(str(self.args.version))} yum install '{project}-{self.args.version}' -y", ".")
         except:
             raise Exception('Failed to install Opensearch')
         return True
