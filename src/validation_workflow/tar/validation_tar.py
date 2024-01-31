1d0b0502b13756a0e93d1886a5f3701a4b1c81bf
Divya Madala <divyaasm@amazon.com>
2024-01-31 07:42:30 -0800

    test
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/validation_workflow/tar/validation_tar.py
+++ src/validation_workflow/tar/validation_tar.py
@@ -35,10 +35,11 @@ class ValidateTar(Validation, DownloadUtils):
             if (isFilePathEmpty):
                 if ("https:" not in self.args.file_path.get(project)):
                     self.copy_artifact(self.args.file_path.get(project), str(self.tmp_dir.path))
-                    version = self.version_parser(self.args.file_path.get("opensearch"))
-                    logging.info(version)
                 else:
                     self.check_url(self.args.file_path.get(project))
+                    logging.info("version from the url")
+                    version = self.version_parser(self.args.file_path.get("opensearch"))
+                    logging.info(version)
             else:
                 if (self.args.artifact_type == "staging"):
                     self.args.file_path[project] = f"{self.base_url_staging}{project}/{self.args.version}/{self.args.build_number[project]}/linux/{self.args.arch}/{self.args.distribution}/dist/{project}/{project}-{self.args.version}-linux-{self.args.arch}.tar.gz"  # noqa: E501
