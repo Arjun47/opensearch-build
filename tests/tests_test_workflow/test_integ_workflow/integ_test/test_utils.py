d7f52221d78b2054c4fb17343bf4671396ef2840
Divya Madala <divyaasm@amazon.com>
2024-02-08 08:09:56 -0800

    Add changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- tests/tests_test_workflow/test_integ_workflow/integ_test/test_utils.py
+++ tests/tests_test_workflow/test_integ_workflow/integ_test/test_utils.py
@@ -6,11 +6,19 @@
 # compatible open source license.
 
 import unittest
+import base64
 
-from test_workflow.integ_test.utils import get_password
+from test_workflow.integ_test.utils import get_password, str_to_base64
 
 
 class TestUtils(unittest.TestCase):
     def test_strong_password(self) -> None:
         self.assertEqual("admin", get_password("2.11.1"))
         self.assertEqual("myStrongPassword123!", get_password("3.0.0"))
+        self.assertEqual("YWRtaW4=", get_password("2.11.1", True))
+        self.assertEqual("bXlTdHJvbmdQYXNzd29yZDEyMyE=", get_password("2.12.0", True))
+
+    def test_str_to_base64(self):
+        value = "admin"
+        result = base64.b64encode(value.encode("utf-8")).decode("utf-8")
+        self.assertEqual(str_to_base64(value), result)
